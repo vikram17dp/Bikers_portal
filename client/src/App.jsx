a53fd65958632ed7cb70ce76c368f143f764df02
vikram17dp <vikramdp505@gmail.com>
2024-10-18 02:40:29 +0530

    some changes relate d to the files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Header from './Components/Header/Header'
-import NavBar from './Components/NavBar/NavBar'
+
 
 function App() {
   return (
     <div>
-      <NavBar/>
- 
+        
     </div>
   )
 }
