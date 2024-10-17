9b0e87b9bb9b7805176db17d737b1f5052743b1a
vikram17dp <vikramdp505@gmail.com>
2024-10-18 03:23:23 +0530

    adding the navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { assets } from '../assets/assets';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className="bg-gray-600 h-16 flex items-center justify-between px-4">
+      <div>
+        <Link to='/'>
+        <img src={assets.Logo}  className="w-20 h-14 rounded-full ml-12" alt="Logo" />
+        </Link>
+      </div>
+      <div className="flex-grow flex justify-center">
+        <div className="flex space-x-24">
+          <Link to="/" className="text-white">Home</Link>
+          <Link to="/about" className="text-white">About</Link>
+          <Link to="/contact" className="text-white">Contact</Link>
+          <Link to="/signin" className='text-white'>Signin</Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
