2567af7a4fa89d373b1eb044c7da9bab876410c8
vikram17dp <vikramdp505@gmail.com>
2024-11-15 15:45:32 +0530

    small changes in these files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; 
@@ -12,19 +11,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </AppContextProvider>
   </BrowserRouter>,
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
-import AppContextProvider from "./context/AppContext.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <AppContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </AppContextProvider>
->>>>>>> 6e38898927702cfe2446767be0ed2aaae697d19c
 );
