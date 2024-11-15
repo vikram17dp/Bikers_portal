6e38898927702cfe2446767be0ed2aaae697d19c
vikram17dp <vikramdp505@gmail.com>
2024-11-15 14:01:10 +0530

    adding the context to the client side

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; 
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import AppContextProvider from "./context/AppContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AppContextProvider>
     <BrowserRouter>
-
-
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </AppContextProvider>
 );
