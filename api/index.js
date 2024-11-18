d74c787b89669bf1aae72a88106d079226b1c8b3
vikram17dp <vikramdp505@gmail.com>
2024-11-18 18:56:14 +0530

    adding the cors to the index.js file

--- api/index.js
+++ api/index.js
@@ -13,7 +13,10 @@ connectCloudinary();
 
 const port = process.env.PORT || 4000;
 
-app.use(cors())
+app.use(cors({
+    origin: '*',
+  }));
+  
 app.use('/api/user',userRouter)
 app.use('/api/admin',adminRouter)
 
